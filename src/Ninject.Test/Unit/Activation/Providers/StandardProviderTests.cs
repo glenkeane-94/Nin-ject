ninject/Ninject

Refactor part of Exceptionformatter into a component (#308)
* Refactor (part of) ExceptionFormatter as a component.

* Pass IExceptionFormatter to Context.ctor().

* Moved ExceptionFormatter to Components namespace.

* Update namespace of (I)ExceptionFormatter.

src/Ninject.Test/Unit/Activation/Providers/StandardProviderTests.cs
@@ -2,6 +2,8 @@
 using Ninject.Activation;
 using Ninject.Activation.Caching;
 using Ninject.Activation.Providers;
+using Ninject.Components;
+using Ninject.Infrastructure.Introspection;
 using Ninject.Injection;
 using Ninject.Parameters;
 using Ninject.Planning;
@@ -404,7 +406,8 @@ namespace Ninject.Test.Unit.Activation.Providers
                                       binding,
                                       kernelConfiguration.Components.Get<ICache>(),
                                       kernelConfiguration.Components.Get<IPlanner>(),
-                                      kernelConfiguration.Components.Get<IPipeline>());
+                                      kernelConfiguration.Components.Get<IPipeline>(),
+                                      kernelConfiguration.Components.Get<IExceptionFormatter>());
             context.Parameters = parameters.ToArray();
             return context;
         }

glenkeane-94/Nin-ject

src/Ninject.Test/Unit/Activation/Providers/StandardProviderTests.cs
@@ -2,6 +2,8 @@
 using Ninject.Activation;
 using Ninject.Activation.Caching;
 using Ninject.Activation.Providers;
+using Ninject.Components;
+using Ninject.Infrastructure.Introspection;
 using Ninject.Injection;
 using Ninject.Parameters;
 using Ninject.Planning;
@@ -402,7 +404,8 @@ namespace Ninject.Test.Unit.Activation.Providers
                                       binding,
                                       kernel.Components.Get<ICache>(),
                                       kernel.Components.Get<IPlanner>(),
-                                      kernel.Components.Get<IPipeline>());
+                                      kernel.Components.Get<IPipeline>(),
+                                      kernel.Components.Get<IExceptionFormatter>());
             context.Parameters = parameters.ToArray();
             return context;
         }